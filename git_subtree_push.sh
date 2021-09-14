c34e9a990ff074365fd3cbe59877dfddda614099
Cullen Lee <a410615903@gmail.com>
2021-09-13 18:42:40 -0700

    Roll backed Updated git subtree process

--- git_subtree_push.sh
+++ git_subtree_push.sh
@@ -1,1 +1,1 @@
-git subtree push --prefix packages/utilities https://github.com/keyskull/utilities.git main --squash
+git subtree push --prefix packages/utilities https://github.com/keyskull/utilities.git main
